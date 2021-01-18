0917ac966f321b923850cee7a2b170fecac99704
Brandon Lai <brandon.lai@vanderbilt.edu>
2021-01-18 01:41:16 -0600

    setup frontend join/create room pages

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import JoinRoomPage from './JoinRoomPage';
+import CreateRoomPage from './CreateRoomPage';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <h1>Home Page</h1>
+                    </Route>
+                    <Route path='/join' component={JoinRoomPage} />
+                    <Route path='/create' component={CreateRoomPage} />
+                </Switch>
+            </Router>
+        )
+    }
+}
